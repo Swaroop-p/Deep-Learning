c14f009de80f7a6004bfa8c7b09fd060105ea880
Swaroop-P <soupspring47@gmail.com>
2017-09-02 01:43:03 +0530

    Python code to segregate all train images into 25 folders each of which is the unique label identifier

---
+++ image_segregator.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  2 00:46:21 2017
+
+@author: Swaroop.Padala
+"""
+
+#from multiprocessing import Pool
+import os, sys
+import csv
+from shutil import copyfile
+
+
+
+def main():
+    directories = ["beans","cake","candy","cereal","chips","chocolate","coffee","corn","fish","flour","honey","jam","juice","milk","nuts","oil","pasta","rice","soda","spices","sugar","tea","tomatosauce","vinegar","water"]
+    root = "C:/Users/Swaroop.Padala/Desktop/HackerEarth_IndiaHacks_ML/a0409a00-8-dataset_dp/"    
+    path1 = "train_img/"   
+    #path2 = ""  
+    count = 0
+    for directory in directories:
+        if not os.path.exists(root +"\\segregated_images\\" +directory):
+            os.makedirs(root+"\\segregated_images\\"+ directory)
+        
+    train_images_listing = os.listdir(root + path1)   
+    
+    reader = csv.reader(open(root + 'train.csv', 'r'))
+    d = {}
+    for row in reader:
+       k, v = row
+       d[k+".png"] = v
+    
+    for image in train_images_listing:
+        copyfile(root + path1 + image, root +"\\segregated_images\\" + d[image]+"\\" + image) 
+        count += 1
+        print (count)
+        
+if __name__ == "__main__":
+    main()
